b8666e67ed809984ab21fc71744e64c358669f17
Dan Macbook <dan@go.com>
2015-01-25 00:58:21 -0500

    Nightl checkin - creating basic AndroidManifest files

--- extractor.sh
+++ extractor.sh
@@ -1,18 +1,26 @@
 #!/bin/bash          
 
-
-ls
 ### Main File Location
 ##		Will have to loop through all of this
 #inputLoc=testGit/sm_Tinfoil-Facebook/
-inputLoc=testGit/dantest/
+
+
+appName=danTest
+inputLoc=testGit/$appName/
 
 ### File to Search For
 searchFile=AndroidManifest.xml
 
-# main output location
+# Script location
+MainScriptLoc=`pwd`
+
 ### FIX THIS - Make it automatic
-mainOutput=/Users/dxkvse/Desktop/VersionControlExtractor/mainOutput/
+#scriptPath=/Users/dxkvse/Desktop/VersionControlExtractor/
+
+# main output location
+mainOutput=$MainScriptLoc/mainOutput/
+
+db=$MainScriptLoc/db/AndrosecDatabase.sqlite
 
 rm -rf $mainOutput
 mkdir -p $mainOutput
@@ -21,9 +29,22 @@ mkdir -p $mainOutput
 ### Output Location
 #mkdir -p Output
 
-# Script location
-##	Used to revert back to main script location after running
-MainScriptLoc=`pwd`
+
+
+
+### Logging
+#date1=$(date +"%s") ## Start date of the script
+#logLocation=logs/toolsScript.log
+#touch $logLocation
+#echo "Start Getting Signing Key:" `date` >> $logLocation
+#### Log the conclusion time
+#date2=$(date +"%s") 
+#diff=$(($date2-$date1))
+#echo "toolsScript Total Running Time $(($diff / 60)) minutes and $(($diff % 60)) seconds."  >> $logLocation
+#echo "toolsScript End:" `date` >> $logLocation
+
+
+
 
 ### Database namew & location where information will be written to
 # 
@@ -39,39 +60,11 @@ howManyWordsInString() { echo $#; }
 ### Get information about a specific commit
 getCommitInfo() {
 
-								# testGit/sm_Tinfoil-Facebook//Tinfoil-for-Facebook/src/main
-								#echo hi
-								#echo $1
-								#pwd
-								#	mkdir -p $1
-									#echo $1 $2
-								#	echo $2
-									
-								#	pwd
-									## complains about errors, but I think it still works
-
-
-									#git cherry-pick -r $1 
-									#git checkout --theirs -- AndroidManifest.xml  ### Pulls in their version of file
-								#	git checkout --theirs -- AndroidManifest.xml  ### Pulls in their version of file
-									#mkdir -p $mainOutput/Tinfoil-for-Facebook/$1
-									#git checkout $1
-									#git merge -s ours
-									
-
-
-								#git reset --merge
-	git checkout $1 .
-	mkdir -p $mainOutput/danTest/$1
-								#cp AndroidManifest.xml $mainOutput/Tinfoil-for-Facebook/$1
-
+	git checkout $2 .
+	mkdir -p $mainOutput/$1/$2
 
 	## Move the ManifestFile to another location
-	cp AndroidManifest.xml $mainOutput/danTest/$1
-	
-
-
-
+	cp AndroidManifest.xml $mainOutput/$1/$2	
 }
 
 
@@ -82,71 +75,84 @@ getCommitInfo() {
 ############################################# 
 
 
-# Find the file location
-FileLoc=`find $inputLoc -name AndroidManifest.xml`
-
-# Log all versions of the file
-cd $(dirname $FileLoc)
 
 
 
-## Remove temp file if it exists
-##	Done here to leave it from troubleshooting after run
-rm -f temp2.txt
+### Add the Info to SQLiteDB
 
-#ManifestCommit=`git log --follow AndroidManifest.xml` 
-	#echo $ManifestCommit
+		### Add the appName
 
+#sqlite3 $db "UPDATE ToolResults SET DefectCount=$defectCount WHERE ApkId=$rowid;"
+sqlite3 $db "select * from appdata;"
 
-
-### A file and not variable is used since it is easier to parse
-git log --follow AndroidManifest.xml>>temp2.txt
-
-
-COUNTER=0
-
-# Read through the filename
-while read p; do
- 
-
-
- 	### Get the commit hashes
-	if [ "${p/commit}" != "$p" ] ; then
-		p=${p//commit/""}
-    		### Ensure that only the commit_hash are added to array, not comments
-    		###	
-			wordcount=`howManyWordsInString $p`
-			if [ "$wordcount" -eq 1 ] ; then
-				#echo ""
-				## Add item to array
-				#echo $p
-				getCommitInfo $p #$(dirname $FileLoc)
-#				exit
-				#commitArray+=($p)
-			fi
-	fi 
-
-done <temp2.txt
+		### Get the AppID
 
 
 
 
-# 785b25cdb4c69e40b048f6eccd399969631f87f6
-#getCommitInfo 785b25cdb4c69e40b048f6eccd399969631f87f6
+	## Do  for every folder in the input directory
+	### put the for loop here
+
+		# Find the file location
+		FileLoc=`find $inputLoc -name AndroidManifest.xml`
+
+		# Log all versions of the file
+		cd $(dirname $FileLoc)
+
+
+
+		## Remove temp file if it exists
+		##	Done here to leave it from troubleshooting after run
+		rm -f temp2.txt
+
+		#ManifestCommit=`git log --follow AndroidManifest.xml` 
+			#echo $ManifestCommit
+
+
+
+		### A file and not variable is used since it is easier to parse
+		git log --follow AndroidManifest.xml>>temp2.txt
+
+
+
+		COUNTER=0
+
+		# Read through the filename
+		while read p; do
+
+
+
+		 	### Get the commit hashes
+			if [ "${p/commit}" != "$p" ] ; then
+				p=${p//commit/""}
+		    		### Ensure that only the commit_hash are added to array, not comments
+		    		###	
+					wordcount=`howManyWordsInString $p`
+					if [ "$wordcount" -eq 1 ] ; then
+
+
+						#echo $(dirname $FileLoc)
+
+						## Add the Commit Info to SQLite
+
+
+						getCommitInfo $appName $p #$(dirname $FileLoc)
+						#commitArray+=($p)
+					fi
+			fi 
+
+		done <temp2.txt
 
 
 
 
-
-
-# exit
 
 ### Loop through the contents of the commit messages array
-COUNTER=0
-for item in "${commitArray[@]}"
-	do
+#COUNTER=0
+#for item in "${commitArray[@]}"
+#	do
 		# Leave in for debugging
-    	let COUNTER=COUNTER+1
+ #   	let COUNTER=COUNTER+1
     	#echo "$COUNTER $item"
 
     	## Cherry pick each commit
@@ -154,7 +160,7 @@ for item in "${commitArray[@]}"
 
 
 
-done
+#done
 
 
 # Revert back to the main script location
