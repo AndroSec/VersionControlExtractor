142c55f64c9bb2cb58df66a084a17b4407f2aea4
dan7800@yahoo.com <dan7800@yahoo.com>
2015-01-27 20:19:56 +0000

    working version of extractor before final cleanup

--- extractor.sh
+++ extractor.sh
@@ -71,11 +71,11 @@ getCommitInfo() {
 
 	### Add the commit info to the database
 	## First check to see if it exists, and if so then update the commit info
-	CommitCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_commitinfo WHERE AppID='$appID';"`  
+	CommitCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_commitinfo WHERE AppID='$appID' and commit_val='$Commit_Val';"`  
 
 	### Check to see if the commit information had yet been entered
 	if [[ $CommitCount -eq 0 ]]; then
-		echo "Enter Initial Commit info for: " $Commit_Val
+		echo "Enter Initial Commit info for: " $appName "-" $Commit_Val
 		sqlite3 $db "INSERT INTO Android_Manifest_commitinfo (Commit_Val, AppID, Commit_Order) VALUES ('$Commit_Val', $appID, $CommitOrder);"
 
 	fi
@@ -112,91 +112,67 @@ getCommitInfo() {
   		## Get the app Name
 		appName=${f//$projectFiles/""}
 		appName=${appName//\//""}
-  		echo $appName
+  		#echo $appName
+
+	    ## Check to see if AppInfo exists & insert it if needed
 
 
 
+	    AppInfoCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`  
+		if [[ $AppInfoCount -eq 0 ]]; then
+			echo "Enter Android_Manifest_AppInfo: " $appName
+			sqlite3 $db  "INSERT INTO Android_Manifest_AppInfo (AppName) VALUES ('$appName');"
+		fi
 
-  		#echo $bas
-  	
-
-		#appName=danTest
-		#inputLoc=testGit/$appName/
-
-
-
+		AppID=`sqlite3 $db "SELECT AppID FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`
 
 
-
-
-
-  	done
+		## Do  for every folder in the input directory
+		### put the for loop here
 
 
 
-
-
-exit
-    ## Check to see if AppInfo exists & insert it if needed
-
-     AppInfoCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`  
-	if [[ $AppInfoCount -eq 0 ]]; then
-		sqlite3 $db  "INSERT INTO Android_Manifest_AppInfo (AppName) VALUES ('$appName');"
-	fi
-
-	AppID=`sqlite3 $db "SELECT AppID FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`
-
-
-
-	## Do  for every folder in the input directory
-	### put the for loop here
 
 		# Find the file location
-		FileLoc=`find $inputLoc -name AndroidManifest.xml`
+		FileLoc=`find $f -name AndroidManifest.xml`
 
 		# Log all versions of the file
 		cd $(dirname $FileLoc)
 
-
-
-		## Remove temp file if it exists
-		##	Done here to leave it from troubleshooting after run
+			## Remove temp file if it exists
+			##	Done here to leave it from troubleshooting after run
 		rm -f temp2.txt
 
-		#ManifestCommit=`git log --follow AndroidManifest.xml` 
-			#echo $ManifestCommit
 
 
+			#ManifestCommit=`git log --follow AndroidManifest.xml` 
+				#echo $ManifestCommit
 
-		### A file and not variable is used since it is easier to parse
+			### A file and not variable is used since it is easier to parse
 		git log --follow AndroidManifest.xml>>temp2.txt
-
-
 
 		COUNTER=1
 
 		# Read through the filename
 		while read p; do
 
-
-
 		 	### Get the commit hashes
 			if [ "${p/commit}" != "$p" ] ; then
 				p=${p//commit/""}
-		    		### Ensure that only the commit_hash are added to array, not comments
-		    		###	
-					wordcount=`howManyWordsInString $p`
-					if [ "$wordcount" -eq 1 ] ; then
+		    	### Ensure that only the commit_hash are added to array, not comments
+		    	###	
+				wordcount=`howManyWordsInString $p`
+				if [ "$wordcount" -eq 1 ] ; then
 
 
-						#echo $(dirname $FileLoc)
+					#echo $(dirname $FileLoc)
 
-						## Add the Commit Info to SQLite
+					## Add the Commit Info to SQLite
 
 
-						getCommitInfo $appName $p $COUNTER $AppID #$(dirname $FileLoc)
-						#commitArray+=($p)
-					fi
+					getCommitInfo $appName $p $COUNTER $AppID #$(dirname $FileLoc)
+					#commitArray+=($p)
+				fi
 			fi 
 
 		done <temp2.txt
@@ -205,28 +181,31 @@ exit
 
 
 
-### Loop through the contents of the commit messages array
-#COUNTER=0
-#for item in "${commitArray[@]}"
-#	do
-		# Leave in for debugging
- #   	let COUNTER=COUNTER+1
-    	#echo "$COUNTER $item"
+	### Loop through the contents of the commit messages array
+	#COUNTER=0
+	#for item in "${commitArray[@]}"
+	#	do
+			# Leave in for debugging
+	 #   	let COUNTER=COUNTER+1
+	    	#echo "$COUNTER $item"
 
-    	## Cherry pick each commit
-    	#git cherry-pick $item
-
-
-
-#done
-
-
-# Revert back to the main script location
-cd $MainScriptLoc
+	    	## Cherry pick each commit
+	    	#git cherry-pick $item
 
 
 
+	#done
 
+
+	# Revert back to the main script location
+	cd $MainScriptLoc
+
+
+	done
+
+
+
+  
 
 ### Now analyze all of the output AndroidManifestFiles to gather information from them.
 
