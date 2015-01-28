dd4b184602a4802ec11e910b948a595b58a8ca45
dan7800@yahoo.com <dan7800@yahoo.com>
2015-01-28 16:36:57 +0000

    mostly working, order is still reversed

--- extractor.sh
+++ extractor.sh
@@ -1,22 +1,13 @@
 #!/bin/bash          
 
-	### Main File Location
-	##		Will have to loop through all of this
-	#inputLoc=testGit/sm_Tinfoil-Facebook/
-
-
-	### NEED A WAY TO FIGURE THIS OUT
-	#appName=danTest
-	#inputLoc=testGit/$appName/
+	# Author: Daniel E. Krutz
+	# Description: Extracts all versions of AndroidManifest.xml files from all supplied GIT repos.
 
 	### File to Search For
 	searchFile=AndroidManifest.xml
 
 	# Script location
 	MainScriptLoc=`pwd`
-
-	### FIX THIS - Make it automatic
-	#scriptPath=/Users/dxkvse/Desktop/VersionControlExtractor/
 
 	# main output location
 	mainOutput=$MainScriptLoc/mainOutput/
@@ -36,16 +27,7 @@
 	rm -f $logLocation ## Clear the log file if it exists
 	touch $logLocation
 	echo "Start Extractor: " `date` >> $logLocation
-	#echo "Start Getting Signing Key:" `date` >> $logLocation
-	#### Log the conclusion time
-	#date2=$(date +"%s") 
-	#diff=$(($date2-$date1))
-	#echo "toolsScript Total Running Time $(($diff / 60)) minutes and $(($diff % 60)) seconds."  >> $logLocation
-	#echo "toolsScript End:" `date` >> $logLocation
-
-	### Database namew & location where information will be written to
-	# 
-
+	
 	#############################################
 	##### Functions
 
@@ -55,7 +37,11 @@
 
 
 ## Just do temp
+echo "Temp - Removing all db information"
 sqlite3 $db "delete from Android_Manifest_commitinfo"	
+sqlite3 $db "delete from Android_Manifest_Appinfo"	
+sqlite3 $db "delete from Android_Manifest_Permission"	
+sqlite3 $db "delete from Android_Manifest_Permission_join"	
 
 
 	# Loop through all of the project files
@@ -71,7 +57,6 @@ sqlite3 $db "delete from Android_Manifest_commitinfo"
   		#echo $appName
 
 	    ## Check to see if AppInfo exists & insert it if needed
-
 	    AppInfoCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`  
 		if [[ $AppInfoCount -eq 0 ]]; then
 #			echo "Enter Android_Manifest_AppInfo: " $appName
@@ -81,35 +66,25 @@ sqlite3 $db "delete from Android_Manifest_commitinfo"
 
 		AppID=`sqlite3 $db "SELECT AppID FROM Android_Manifest_AppInfo WHERE AppName='$appName';"`
 
-
-		## Do  for every folder in the input directory
-		### put the for loop here
-
 		# Find the file location
 		FileLoc=`find $f -name AndroidManifest.xml`
 
-		### Check to see if the File is found
-
-		#echo $FileLoc
-		#if [ $FileLoc="" ] ; then
+		### Check to see if the AndroidManifestFile is found
 		if [ -z "$FileLoc" ]; then
 			echo "No file found for $appName"
 			echo "***No Android Manifest File found for: $appName: " `date` >> $logLocation
-
 		else
 			echo "***Android Manifest File found for: $appName - $FileLoc: " `date` >> $logLocation
-#			echo "File Found for $appName"
-			# Log it
-	
-			# Log all versions of the file
+			
+			## Move to the appropriate directory
 			cd $(dirname $FileLoc)
 
 			## Remove temp file if it exists
 			##	Done here to leave it from troubleshooting after run
-#			rm -f temp2.txt
+			rm -f temp2.txt
 
 			### A file and not variable is used since it is easier to parse
-#			git log --follow AndroidManifest.xml>>temp2.txt
+			git log --follow AndroidManifest.xml>>temp2.txt
 
 			COUNTER=0
 			LineCounter=0
@@ -127,16 +102,24 @@ sqlite3 $db "delete from Android_Manifest_commitinfo"
 					## 		Kind of an ugly way to do things, but it works
 					tempLine=$((LineCounter + 1)) 
 					AuthorInfo=`sed -n "${tempLine}p" < temp2.txt`
-					#echo $AuthorInfo
-
 
 					#### Split up the information to get author name and author email
-					Author_Name="tempName"
-					Author_Email="tempEmail"
+					# Remove the text Author:
+					AuthorInfo=${AuthorInfo//Author:/""}
 
+					IFS='<' read -a array <<< "$AuthorInfo"
+
+					Author_Name="${array[0]}"
+					Author_Name=${Author_Name// /""} 
+
+					Author_Email="${array[1]}"
+					Author_Email=${Author_Email//>/""} 
+			
 					## Date info
 					tempLine=$((LineCounter + 2)) 
 					DateInfo=`sed -n "${tempLine}p" < temp2.txt`
+					DateInfo=${DateInfo//Date: /""} 
+					#DateInfo=${DateInfo// /""} 
 					#echo $DateInfo
 
 					## Message info
@@ -144,59 +127,31 @@ sqlite3 $db "delete from Android_Manifest_commitinfo"
 					MessageInfo=`sed -n "${tempLine}p" < temp2.txt`
 					#echo $MessageInfo
 
+
+					MessageInfo=${MessageInfo// /""} 
+
 	  				### Ensure that only the commit_hash are added to array, not comments
 					wordcount=`howManyWordsInString $p`
 					if [ "$wordcount" -eq 1 ] ; then
 
-						#echo $(dirname $FileLoc)
-						## Add the Commit Info to SQLite
-
-						#getCommitInfo $appName $p $COUNTER $AppID $Author_Name $Author_Email $DateInfo $MessageInfo #$(dirname $FileLoc)
-						#commitArray+=($p)
-
-
-
 						CommitCount=`sqlite3 $db "SELECT count(*) FROM Android_Manifest_commitinfo WHERE AppID='$AppID' and commit_val='$Commit_Val';"`  
-						#echo $CommitCount
 
 						### Check to see if the commit information had yet been entered
 						if [[ $CommitCount -eq 0 ]]; then
 							COUNTER=$[COUNTER + 1]
-	#						echo "Enter Initial Commit info for: " $appName "-" $Commit_Val
-	#						sqlite3 $db "INSERT INTO Android_Manifest_commitinfo (Commit_Val, AppID, Commit_Order, Author_Name, Author_Email, Commit_Date, Commit_Message) VALUES ('$Commit_Val', $AppID, $COUNTER, '$Author_Name', '$Author_Email', '$DateInfo', '$MessageInfo');"
+							echo "***Android_Manifest_commitinfo Insert: $appName $Commit_Val " `date` >> $MainScriptLoc/$logLocation
+							echo "Enter Initial Commit info for: " $appName "-" $Commit_Val
+							sqlite3 $db "INSERT INTO Android_Manifest_commitinfo (Commit_Val, AppID, Commit_Order, Author_Name, Author_Email, Commit_Date, Commit_Message) VALUES ('$Commit_Val', $AppID, $COUNTER, '$Author_Name', '$Author_Email', '$DateInfo', '$MessageInfo');"
 						fi
 
-						#echo $appName
-
-		#echo $Commit_Val
 					
-	
+						## Pull the value from the GIT repo
 						git checkout $Commit_Val .
 	
 						mkdir -p $mainOutput/$appName/$Commit_Val
 
-
-						#echo $mainOutput/$appName/$Commit_Val
-
-
-					
-	#pwd
-						## Move the ManifestFile to another location
+						## Move the pulled manifest file to the main output location
 						cp AndroidManifest.xml $mainOutput/$appName/$Commit_Val	
-
-						## Copies the manifest file to the local folder with the 
-		#				cp AndroidManifest.xml $mainOutput/$appName/$Commit_Val	
-
-						## Move the created folder to the main output directory
-					
-
-
-exit
-
-			### Add logging
-			### test
-
-
 					fi
 				fi 
 
@@ -208,10 +163,6 @@ exit
 
 		done
 
-
-
-
-
 	diff=$(($date2-$date1))
 	echo "Total Running Time $(($diff / 60)) minutes and $(($diff % 60)) seconds."  >> $logLocation
 	echo "End:" `date` >> $logLocation
@@ -220,9 +171,8 @@ exit
 
 
 #### Todo
-# Logging
-# Author, Date, Commit message
-# Parse the date/time information
+# Commit message should be able to handle multiple lines
+# Commit_Order is reversed
 
 ### To check and make sure that all values are being logged properly
 
