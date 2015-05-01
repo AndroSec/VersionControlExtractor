dc806224708bde465758e149b819ac6c0e7e0b20
Dan Macbook <dan@go.com>
2015-05-01 17:50:48 -0400

    initial version before writing script

---
+++ getPermissionFromAPK.sh
@@ -0,0 +1,20 @@
+#!/bin/bash    
+
+### Description
+##		Get the permission information from each APK file
+
+	## Location of APkFiles files
+	outputFiles=mainOutput/
+
+
+	## DB location
+	db=$MainScriptLoc/db/AndrosecDatabase.sqlite
+
+
+
+	echo hello
+
+	
+
+
+	### Todo
