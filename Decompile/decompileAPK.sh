5df5d9174a964e1dd7452972d5d10ab6f0311eec
Dan Macbook <dan@go.com>
2015-05-02 09:50:57 -0400

    random checkin

--- Decompile/decompileAPK.sh
+++ Decompile/decompileAPK.sh
@@ -1,11 +1,20 @@
 #!/bin/bash          
 
-	echo DecompileYo
+###	Leaving as an external file for now in case it needs to be refactored or added to.
+###		My goal is to make these into seperate, reusable components.
 
+	apk_Conv_dir=decompile/tools/
 
 	### Pass in information about file to be deompiled
 
 	### Information to be passed in
-	echo $1
-	echo $2
-	echo $3
+	#echo $1 # AppID
+	#echo $2	# VersionID
+	#echo $3 # apkFile
+	#echo $4	# DecompileOuput
+
+	java -jar $apk_Conv_dir/apktool.jar d -f $3 $4
+
+
+
+
