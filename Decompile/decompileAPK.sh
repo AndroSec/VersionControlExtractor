139652454bddaf6316db223bdb5004cb73068133
Dan Macbook <dan@go.com>
2015-05-01 23:29:00 -0400

    nightly checkin

---
+++ Decompile/decompileAPK.sh
@@ -0,0 +1,11 @@
+#!/bin/bash          
+
+	echo DecompileYo
+
+
+	### Pass in information about file to be deompiled
+
+	### Information to be passed in
+	echo $1
+	echo $2
+	echo $3
--- getPermissionFromAPK.sh
+++ getPermissionFromAPK.sh
@@ -4,16 +4,57 @@
 ##		Get the permission information from each APK file
 
 	## Location of APkFiles files
-	outputFiles=mainOutput/
-
+	apkInputDir=../apks_small/
 
 	## DB location
-	db=$MainScriptLoc/db/AndrosecDatabase.sqlite
+	DBName=db/AndrosecDatabase.sqlite
+
+	## Convert location
+	decompile=Decompile/decompileAPK.sh
+
+	#cd $apkInputDir
+	#ls
 
 
+	#### Loop through each APK file
 
-	echo hello
+	## Find the AppID, VersionID from the database
+	##		Create function to do this
 
+	## If found, unzip the file and decompile the contents
+	##		Create external function to do this
+
+	## Find the manifest file and analyze the contents of it
+	
+
+	#$$$$$$$$$
+	FILES=$(find $apkInputDir -maxdepth 1 -type f  -name '*.apk')
+	for f in $FILES
+	do
+		filename=$(basename "$f")
+
+
+		#echo $filename | awk -F'_' '{print $1,$2}'
+
+		#### There is a cleaner way to to do this
+		part1=`echo $filename | awk -F'_' '{print $1}'`
+		part2=`echo $filename | awk -F'_' '{print $2}'`
+		part2=${part2//.apk/""}
+	
+		### Now get the information from the database
+		appID=`sqlite3 $DBName "SELECT appID FROM appdata WHERE name='$part1';"`
+
+
+		### Find the versionID based on Part 2 
+		versionID=`sqlite3 $DBName "SELECT versionID FROM version WHERE appID =$appID and build_number=$part2;"`
+
+		echo $appID
+		echo $versionID
+
+		#### Now convert the file
+		./$decompile $appID $versionID $f
+
+	done
 	
 
 
